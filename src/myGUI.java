c595677670c84f127d3ea712cac7b7f1ae0f05ee
Matthew Becker <becker@tamu.edu>
2020-09-09 22:08:56 -0500

    Initial commit
    Preliminary work on Java interfacing with the MySQL database running on my Raspberry Pi

---
+++ src/myGUI.java
@@ -0,0 +1,40 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class myGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					myGUI frame = new myGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public myGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1080, 720);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
---
+++ src/test1_driver.java
@@ -0,0 +1,9 @@
+// From https://www.tutorialspoint.com/jdbc/jdbc-sample-code.htm
+
+public class test1_driver {
+
+	public static void main(String[] args) {
+		System.out.println("Howdy! Standard out is working.");
+	}
+
+}
