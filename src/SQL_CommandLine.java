05e72d098d07d865bd81c9393344f1f5f47bac5b
Matthew Becker <becker@tamu.edu>
2020-09-23 16:55:45 -0500

    9/23 - Daily work
    See development log for details.

--- src/SQL_CommandLine.java
+++ src/SQL_CommandLine.java
@@ -1,10 +1,49 @@
-
-// Command line, provides a command line interface for the database project
+import java.util.Scanner;
+// Command line, provides a command line interface for the command line interpreter
 public class SQL_CommandLine {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Construct the interpreter object
+		SQL_CommandLineInterpreter CLI = new SQL_CommandLineInterpreter();
 		
+		// Primary command loop
+		Scanner scanner = new Scanner(System.in);
+		String userIn;
+		
+		// User-facing text
+		System.out.println("Note: jdb> is the prompt. \r\n" + 
+						   "Note: all custom commands are prefixed by \"jdb-\".");
+		System.out.println("Note: To exit, enter \"EXIT CLI\"");
+		System.out.println("");
+		
+		while (true) {
+			// Grab user input and make sure it's not empty
+			System.out.print("jdb> ");
+			try {
+				userIn = scanner.nextLine();
+			} catch (Exception e) {
+				continue;
+			}
+			
+			if (userIn.equalsIgnoreCase("EXIT CLI")) {
+				break;
+			}
+	
+			// Provide warning for comments in SQL query, usually resulting from a copy-paste of a query
+			if (userIn.contains("-- ")) {
+				System.out.println("WARNING: Comments in SQL queries can have unexpected consequences");
+			}
+			
+			// At this point user input has been scanned, send to the processor
+			String result = CLI.processInput(userIn);
+			
+			// Print the returned string
+			System.out.println(result);
+			
+			
+		}
+		
+		scanner.close();
+		System.out.println("\n\nThank you for using our DB client. Have a nice day!");
 	}
-
 }
--- src/SQL_JDBC.java
+++ src/SQL_JDBC.java
@@ -1,5 +1,105 @@
+import java.sql.*;
 
 // Utility class to query the database, expects SQL code and returns ResultSets
 public class SQL_JDBC {
+	// Note by setting the serverTimezone argument, date/time stamps may be incorrect
+	private static final String DB_URL = "jdbc:mysql://192.168.1.77:3306/adventureworks?serverTimezone=US/Central";
+	
+	// Not a fan of just having the password in the java file, just saying
+	// The right way to do this is probably referencing environment variables which van be git ignored or asking the user to login them selves
+	private static final String USER = "user2";
+	private static final String PASS = "c8kPA8eHaXsBNEPE";
+	
+	// Holds connection info for the DB, established in the constructor
+	private Connection connection;
 
+	// Status flag: Whether or not this object is connected to the DB, may not be used
+	private boolean connected;
+	
+	
+	public SQL_JDBC() {
+		// Establish DB connection with error handling
+		try {
+			connection = DriverManager.getConnection(DB_URL, USER, PASS);
+			connected = true;
+		} catch (SQLException e) {
+			System.out.println("ERROR: SQL EXCEPTION WHILE CONNECTING TO DATABASE");
+			System.out.println(e);
+			System.out.println(e.getStackTrace());
+			connection = null;
+			connected = false;
+		} catch (Exception e) {
+			System.out.println("ERROR: EXCEPTION WHILE CONNECTING TO DATABASE");
+			System.out.println(e);
+			System.out.println(e.getStackTrace());
+			connection = null;
+			connected = false;
+		}
+	}
+	
+	public ResultSet query(String query) {
+		ResultSet rs = null;
+		if (!connected) {
+			return rs;
+		}
+		
+		// Error handling surrounding queries
+		try {
+			// Initialize a statement from the connection
+			Statement st = connection.createStatement();
+			
+			// Execute the query
+			rs = st.executeQuery(query);
+			
+			// Check for null ResultSet and return
+			if (rs == null) {
+				System.out.println("WARNING: null ResultSet returned in SQL_JDBC.query()");
+				System.out.println("QUERY: " + query);
+			}
+			return rs;
+			
+			
+		} catch (SQLException e) {
+			System.out.println("ERROR: SQL EXCEPTION IN SQL_JDBC.query()");
+			System.out.println("QUERY: " + query);
+			System.out.println("Error: " + e);
+			//System.out.println(e.getStackTrace());
+			
+			rs = null;
+			return rs;
+		}
+		
+	}
+	
+	// Returns a DatabaseMetaData object for the connected DB
+	public DatabaseMetaData get_DatabaseMetaData() {
+		DatabaseMetaData md = null;
+		
+		// Try to get the database meta data
+		try {
+			md = connection.getMetaData();
+		} catch (SQLException e) {
+			md = null;
+			System.out.println("ERROR: SQL EXCEPTION IN SQL_JDBC.get_DatabaseMetaData()");
+			System.out.println("Error: " + e);
+		}
+		
+		return md;
+	}
+
+
+	@Override
+	public String toString() {
+		if (connected) {
+			return "SQL_JDBC\n" + "CONNECTED WITH USER: " + USER + "\nON URL: " + DB_URL;
+		} else {
+			return "SQL_JDBC\n" + "NOT CONNECTED TO DB\n";
+		}
+	}
+	
+	// Returns true if the JDBC is connected to the data base
+	public boolean isConnected() {
+		return connected;
+	}
+	
 }
