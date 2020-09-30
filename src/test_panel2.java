f6c0e7773b4f5318403005ed9de4c46001f04591
Matthew Becker <becker@tamu.edu>
2020-09-29 23:17:59 -0500

    Added panel example test_* files

---
+++ src/test_panel2.java
@@ -0,0 +1,24 @@
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class test_panel2 extends JPanel {
+	private final JLabel lblNewLabel = new JLabel("THIS IS TEST_PANEL2");
+
+	/**
+	 * Create the panel.
+	 */
+	public test_panel2() {
+
+		initGUI();
+	}
+	private void initGUI() {
+		setLayout(null);
+		setBounds(100, 100, 1080, 720);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		lblNewLabel.setBounds(233, 175, 608, 257);
+		
+		add(lblNewLabel);
+	}
+}
