5590c5bd7da00e38426245897edb292dbbc8fc8e
nanhuang <nanhuang@tamu.edu>
2020-10-04 20:58:20 -0500

    Add files via upload

--- src/showAllPrimaryKeys.java
+++ src/showAllPrimaryKeys.java
@@ -1,3 +1,57 @@
+<<<<<<< HEAD
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextArea;
+
+public class showAllPrimaryKeys extends JPanel {
+	
+	private SQL_CommandLineInterpreter Keys;
+	
+	public showAllPrimaryKeys(SQL_CommandLineInterpreter CLI) {
+		setLayout(null);
+		
+		JLabel lblNewJgoodiesTitle = DefaultComponentFactory.getInstance().createTitle("Show All Primary Keys");
+		lblNewJgoodiesTitle.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblNewJgoodiesTitle.setBounds(332, 55, 308, 35);
+		add(lblNewJgoodiesTitle);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 147, 909, 367);
+		add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		Keys=CLI;
+		
+		JButton btnNewButton = new JButton("Show");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.setText(CLI.processInput("jdb-show-all-primary-keys"));
+				textArea.setCaretPosition(0); //0 will ensure the scroll bar isn't initialized to the bottom of the text area
+				textArea.setVisible(true);
+				scrollPane.setVisible(true);
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnNewButton.setBounds(379, 101, 148, 35);
+		add(btnNewButton);
+		
+
+	
+		
+		
+
+	}
+}
+=======
 
 import javax.swing.JPanel;
 
@@ -11,3 +65,4 @@ public class showAllPrimaryKeys extends JPanel {
 	}
 
 }
+>>>>>>> 4e3dc40093763bc66f77babce1a903f0ec76e0ab
--- src/showRelatedTables.java
+++ src/showRelatedTables.java
@@ -1,3 +1,77 @@
+<<<<<<< HEAD
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import java.awt.Color;
+import javax.swing.JTextArea;
+import java.awt.Font;
+import javax.swing.JScrollPane;
+
+public class showRelatedTables extends JPanel {
+private SQL_CommandLineInterpreter Table;
+private JTextField textField;
+private JTextArea textArea = new JTextArea();	
+
+	public showRelatedTables(SQL_CommandLineInterpreter CLI) {
+		setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(38, 103, 228, 37);
+		add(textField);
+		textField.setColumns(10);
+		Table = CLI;
+		
+		//String INput = textField.getText();
+		
+		JLabel lblNewLabel = new JLabel("Table Name: ");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setBounds(38, 76, 142, 20);
+		add(lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(523, 93, 417, 400);
+		add(scrollPane);
+		
+		//JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		textArea.setEditable(false);
+		textArea.setCaretPosition(0); //0 will ensure the scroll bar isn't initialized to the bottom of the text area
+		textArea.setVisible(true);
+		scrollPane.setVisible(true);
+		
+		JLabel lblNewJgoodiesTitle = DefaultComponentFactory.getInstance().createTitle("All Related Tables :");
+		lblNewJgoodiesTitle.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		lblNewJgoodiesTitle.setBounds(523, 45, 309, 37);
+		add(lblNewJgoodiesTitle);
+		
+		JButton btnNewButton = new JButton("Show");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//textArea.setText(CLI.processInput("jdb-show-related-tables "+ INput));
+				runSearch();
+	
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnNewButton.setBounds(38, 151, 117, 37);
+		add(btnNewButton);
+	}
+		private void runSearch() {
+			
+			// Grab user input
+			String t1 = textField.getText();			
+			// Pass to CLI as command and display output after processing
+			textArea.setText(Table.processInput("jdb-show-related-tables "+ t1));
+			
+			return;
+		}
+	
+}
+=======
 
 
 import javax.swing.JPanel;
@@ -12,3 +86,4 @@ public class showRelatedTables extends JPanel {
 	}
 
 }
+>>>>>>> 4e3dc40093763bc66f77babce1a903f0ec76e0ab
